d356df86860ba3499839b733952b05699076f9dc
Obot Obo <oboobotenefiok@gmail.com>
2026-04-28 12:48:13 +0100

    Update main.rs
    Overhaul

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,27 @@
-use std::io;
+//Now using one input module against two, shortens it.
 
-struct Body {
-    mass: f64,
-    height: f64,
-}
+mod io_handler;
+mod report;
+mod body;
 
-/*Would shorten but that will defeat original intent to learn Struct & Impl */
-impl Body {
-    fn bmi(&self) -> f64 {
-        (self.mass) / (self.height * self.height)
-    }
-}
+use body::Body;
+
 fn main() {
     println!("What's your height in metres?");
-    let height = get_input();
+    
+    let height = io_handler::get_input();
     println!("You entered height as  {} \n
     Now, what's your weight in Kilograms?", height);
-    let mass = get_input();
+    
+    let mass = io_handler::get_input();
     println!("You entered mass as {}", mass);
     
-    let body_input = Body {
-        mass,
-        height,
-    };
+    let body_input = Body::new(mass, height);
 
     println!(
         "
-    Your Body Mass Index is: {:.2}",
-        body_input.bmi()
+    Your Body Mass Index is: {:.2} \n {}",
+        body_input.bmi(), report::report(body_input.bmi())
     );
 }
 
-//Now using one input function against two, shortens it!
-
-fn get_input() -> f64 {
-   
-    loop { let mut input = String::new();
-        io::stdin()
-        .read_line(&mut input)
-        .expect("We need a number, bro");
-
-  let  input: f64 = match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {println!("Please enter a valid number"); 
-            continue;
-        } ,
-    };
-   return input
-    }
-    
-}
